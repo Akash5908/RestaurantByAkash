386e61fa507dba218547211b89bd3197bde01f72
Akash kumar <venomkr020@gmail.com>
2023-01-15 10:57:33 +0530

    first commit

---
+++ src/component/Basic/menuApi.js
@@ -0,0 +1,86 @@
+const Menu = [
+    {
+        id: 1,
+        image: "/images/banana.jpg",
+        name: "Banana Shake",
+        category: "breakfast",
+        price: "12₹",
+        description:
+            "Banana shakes are often used as a convenient way to incorporate calories and nutrition into a busy lifestyle with the perfect ratio of fruit to ice cream to milk. It's packed full of banana flavor and is pretty much irresistible.Support a health goa",
+    },
+
+    {
+        id: 2,
+        image: "/images/omelette.jpg",
+        name: "Omelette",
+        category: "lunch",
+        price: "20₹",
+        description:
+            "Just in case you haven’t caught on yet, we’re breakfast crazy here at Chicago Waffles! We just love everything about it – from the sweet to the savory, to the healthy and indulgent. And, there are so many great breakfast food items to choose from. But, in this post, we’re talking about ",
+    },
+    {
+        id: 3,
+        image: "/images/blackcoffee.jpg",
+        name: "BlackCoffee",
+        category: "breakfast",
+        price: "10₹",
+        description:
+            "Black coffee is simply coffee that is normally such as sugar, milk, cream, or added flavors. While it has a slightly bitter taste compared to when it is flavored with additives, many people love a strong cup of black coffee. In fact, for some, it is part of their everyday diet. ",
+    },
+    {
+        id: 4,
+        image: "/images/Matarpaneer.jpg",
+        name: "MatarPaneer",
+        category: "evening",
+        price: "50₹",
+        description:
+            "Matar paneer is a traditional North Indian dish of creamy paneer and peas swimming in a mildly spiced tomato sauce. My take on the classic dish can be prepared in just around 30 minutes. ",
+    },
+    {
+        id: 5,
+        image: "/images/jowar.jpg",
+        name: "Jowar Roti",
+        category: "evening",
+        price: "80₹",
+        description:
+            "Jowar roti recipe with step by step pics. These Jowar roti also jowar bhakri flatbreads made with sorghum flour. While the recipe just right, it takes only 30 minutes to make with my step-by-step instructions and photos. ",
+    },
+    {
+        id: 6,
+        image: "/images/Butter.jpg",
+        name: "ButterChicken",
+        category: "dinner",
+        price: "180₹",
+        description:
+            "Butter chicken, traditionally known as murgh makhani (pronounced is an Indian dish originating in New Delhi. It is a type of curry made from chicken with a spiced tomato and butter (makhan) sauce. Its sauce is known for its rich ",
+    },
+    {
+        id: 7,
+        image: "/images/Gulab.jpg",
+        name: "Gulab jamun",
+        category: "dinner",
+        price: "60₹",
+        description:
+            "Gulab jamun lit. 'Rose water berry' or 'Rose berry') is a sweet confectionary or dessert and a type of mithai popular in India, Pakistan, Nepal, the Maldives (where it is known as gulab ki janu) as well as Myanmar. ",
+    },
+    {
+        id: 8,
+        image: "/images/Biryani.jpg",
+        name: "Biryani",
+        category: "lunch",
+        price: "60₹",
+        description:
+            "Biryani is a mixed rice dish originating among the Muslims of the Indian subcontinent. It is made with Indian spices, rice, and usually some type of meat (chicken, beef, goat, lamb, prawn, fish) or in some cases without any meat. ",
+    },
+    {
+        id: 9,
+        image: "/images/Samosa.jpg",
+        name: "Samaso",
+        category: "breakfast",
+        price: "10₹",
+        description:
+            "A samosa or singara is a fried South Asian pastry with a savory filling, including ingredients such as spiced potatoes, onions, and peas. It may take different forms, including triangular, cone. ",
+    },
+];
+
+export default Menu;
